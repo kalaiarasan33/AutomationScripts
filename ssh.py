eb003387824f95147934f8e3be8aba70bf569d05
kalaiarasan33 <48373364+kalaiarasan33@users.noreply.github.com>
2019-07-19 10:14:55 +0530

    Create ssh.py

---
+++ ssh.py
@@ -0,0 +1,16 @@
+import json
+import paramiko
+
+ip='server ip'
+port=22
+username='username'
+password='password'
+
+ssh=paramiko.SSHClient()
+ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+ssh.connect(ip,port,username,password)
+commands = ["ls -al","df -h"]
+for command in commands:
+    stdin, stdout, stderr = c.exec_command(command)
+    print (stdout.read())
+    print (stderr.read())
